da84af6f786514fd30f3ff65f6d1f0cb8c6ddd52
Aleksandr Demchuk <aleksdemchyk@yandex.rugit config --listqgit config --listgit config --global user.email aleksdemchyk@yandex.ru>
2024-05-08 19:14:34 +0300

    работа готова к ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+//Темплейт карточки.
+const cardTemplate = document.querySelector('#card-template').content;
+//DOM узлы.
+const content = document.querySelector('.content');
+const places = content.querySelector('.places');
+const deleteButton = cardTemplate.querySelector('.card__delete-button');
+const placesList = content.querySelector('.places__list');
 
-// @todo: DOM узлы
+//Функция создания карточки.
+function addCard(initialCards, onDelete) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = card.querySelector('.card__delete-button');
+  card.querySelector('.card__title').textContent = initialCards.name;
+  card.querySelector('.card__image').src = initialCards.link;
+  card.querySelector('.card__image').alt = initialCards.name;
 
-// @todo: Функция создания карточки
+  deleteButton.addEventListener('click', () => {
+    onDelete(card);
+  });
 
-// @todo: Функция удаления карточки
+  return card;
+}
 
-// @todo: Вывести карточки на страницу
+//Функция удаления карточки.
+function deleteCard(removeData) {
+  removeData.remove();
+}
+
+//Вывод карточки на страницу.
+function showCards() {
+  for (let i = 0; i < initialCards.length; i++) {
+    const result = addCard(initialCards[i], deleteCard);
+    placesList.append(result);
+  }
+}
+showCards();
