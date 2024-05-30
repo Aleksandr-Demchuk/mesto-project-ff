d813528fadff9b33d87e931d556e780e6eb85eff
Aleksandr Demchuk <aleksdemchyk@yandex.rugit config --listqgit config --listgit config --global user.email aleksdemchyk@yandex.ru>
2024-05-30 23:39:57 +0300

    Работает открытие формы для карточки

--- src/index.js
+++ src/index.js
@@ -16,11 +16,18 @@ const placesList = content.querySelector('.places__list');
 
 
 const profileEdit = document.querySelector('.profile__edit-button');
-const popupTypeEdit = document.querySelector('.popup_type_edit')
+const profileAdd = document.querySelector('.profile__add-button');
+const popupTypeEdit = document.querySelector('.popup_type_edit');
 const closeCrossButton = document.querySelectorAll('.popup__close');
+const openNewPopup = document.querySelector('.popup_type_new-card');
+
 
 profileEdit.addEventListener('click', () => {
   openModal(popupTypeEdit);
+});
+
+profileAdd.addEventListener('click', () => {
+  openModal(openNewPopup);
 });
 
 closeCrossButton.forEach((item) => {
